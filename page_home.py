dbc6cb72fa1d931ee9f3440835335c3a49562749
harejan <julia200594714@gmail.com>
2025-10-22 03:05:39 +0000

    modified:   page_home.py

--- page_home.py
+++ page_home.py
@@ -1,11 +1,4 @@
 import streamlit as st
-# ó¿¿öw
-st.title("歡迎來到GIS專案")
-st.write("這是一個使用 Streamlit 建立的互動專案程式")
-#  MP4 ö URL ó st.video()
-video_url = "https://i.imgur.com/1GoAB0C.mp4"
-st.write(f"正在撥放影片 {video_url}")
-st.video(video_url)
-#  wö URL ó st.image()
-image_url = "https://i.imgur.com/uf1T4ND.png"
-st.image(image_url)
+st.title('洪子晴')
+st.header('洪子晴的GIS專案')
+st.write('這是一個10/22的課堂作業')
